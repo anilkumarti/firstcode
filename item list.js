217b082982dee93957d88d67ec05562a72506b7b
anilkumarti <anil.tiwari1403@gmail.com>
2023-11-20 13:34:39 +0530

    add three files

---
+++ item list.js
@@ -0,0 +1,15 @@
+
+let tittlea=document.getElementById('tittles');
+tittlea.sty
+tittlea.style.cssText='font-size:20;color:green;font-weight:bold;';
+;
+
+
+
+var headerTittle=document.getElementById('main-header');
+headerTittle.style.border='5px solid black';
+headerTittle.style.font="bold green 16px"
+headerTittle.innerText="Main part"
+var list=document.getElementsByClassName('list-group-item');
+
+list[1].textContent="new era";
